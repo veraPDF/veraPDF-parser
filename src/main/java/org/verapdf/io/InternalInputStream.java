3b29e48df3c42fbd509edd060f8767745f814c6f
Timur Kamalov <timur.kamalov@duallab.com>
2015-12-02 14:47:41 +0300

    InternalInputStream.

--- src/main/java/org/verapdf/io/InternalInputStream.java
+++ src/main/java/org/verapdf/io/InternalInputStream.java
@@ -2,36 +2,37 @@ package org.verapdf.io;
 
 import org.verapdf.as.ASInputStream;
 
-import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
 /**
  * @author Timur Kamalov
  */
 public class InternalInputStream implements ASInputStream {
 
-	private InputStream stream;
+	private final static String READ_ONLY_MODE = "r";
 
-	public InternalInputStream(final String fileName) {
-		this.stream = new OurCoolStream(fileName);
+	private RandomAccessFile source;
+
+	public InternalInputStream(final String fileName) throws FileNotFoundException {
+		this.source = new RandomAccessFile(fileName, READ_ONLY_MODE);
 	}
 
-	public int read(byte[] buffer, int size) {
-		this.stream.read(buffer, size);
-		//TODO : count stuff again
-		return this.stream.count;
+	public int read(byte[] buffer, int size) throws IOException {
+		return this.source.read(buffer, 0, size);
 	}
 
-	public int skip(int size) {
-		this.stream.skip(size);
-		return this.stream.count;
+	public int skip(int size) throws IOException {
+		return this.source.skipBytes(size);
 	}
 
-	public void close() {
-		this.stream.close();
+	public void close() throws IOException {
+		this.source.close();
 	}
 
-	public void reset() {
-		this.stream.seek(0);
+	public void reset() throws IOException {
+		this.source.seek(0);
 	}
 
 	public boolean isCloneable() {
