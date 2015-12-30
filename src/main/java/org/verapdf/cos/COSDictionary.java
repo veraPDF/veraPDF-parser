cfdf0e94cd9eef80b4116a18864c7481d52bb976
Timur Kamalov <timur.kamalov@duallab.com>
2015-12-30 14:46:54 +0300

    COSDictionary implementation.

--- src/main/java/org/verapdf/cos/COSDictionary.java
+++ src/main/java/org/verapdf/cos/COSDictionary.java
@@ -13,13 +13,102 @@ public class COSDictionary extends COSDirect {
     private Map<ASAtom, COSObject> entries;
 
     protected COSDictionary() {
+        super();
         this.entries = new HashMap<ASAtom, COSObject>();
+    }
+
+    protected COSDictionary(final ASAtom key, final COSObject value) {
+        this();
+        setKey(key, value);
+    }
+
+    protected COSDictionary(final ASAtom key, final boolean value) {
+        this();
+        setBooleanKey(key, value);
+    }
+
+    protected COSDictionary(final ASAtom key, final int value) {
+        this();
+        setIntegerKey(key, value);
+    }
+
+    protected COSDictionary(final ASAtom key, final double value) {
+        this();
+        setRealKey(key, value);
+    }
+
+    protected COSDictionary(final ASAtom key, final String value) {
+        this();
+        setStringKey(key, value);
+    }
+
+    protected COSDictionary(final ASAtom key, final ASAtom value) {
+        this();
+        setNameKey(key, value);
+    }
+
+    protected COSDictionary(final ASAtom key, final int size, final COSObject[] value) {
+        this();
+        setArrayKey(key, size, value);
+    }
+
+    protected COSDictionary(final ASAtom key, final int size, final double[] value) {
+        this();
+        setArrayKey(key, size, value);
+    }
+
+    protected COSDictionary(final COSDictionary dict) {
+        super();
+        this.entries = dict.entries;
     }
 
     public static COSObject construct() {
         return new COSObject(new COSDictionary());
     }
 
+    public static COSObject construct(final ASAtom key, final COSObject value) {
+        return new COSObject(new COSDictionary(key, value));
+    }
+
+    public static COSObject construct(final ASAtom key, final boolean value) {
+        return new COSObject(new COSDictionary(key, value));
+    }
+
+    public static COSObject construct(final ASAtom key, final int value) {
+        return new COSObject(new COSDictionary(key, value));
+    }
+
+    public static COSObject construct(final ASAtom key, final double value) {
+        return new COSObject(new COSDictionary(key, value));
+    }
+
+    public static COSObject construct(final ASAtom key, final String value) {
+        return new COSObject(new COSDictionary(key, value));
+    }
+
+    public static COSObject construct(final ASAtom key, final ASAtom value) {
+        return new COSObject(new COSDictionary(key, value));
+    }
+
+    public static COSObject construct(final ASAtom key, final int size, final COSObject[] value) {
+        return new COSObject(new COSDictionary(key, size, value));
+    }
+
+    public static COSObject construct(final ASAtom key, final int size, final double[] value) {
+        return new COSObject(new COSDictionary(key, size, value));
+    }
+
+    public static COSObject construct(final COSDictionary dict) {
+        return new COSObject(new COSDictionary(dict));
+    }
+
+    public int size() {
+        return this.entries.size();
+    }
+
+    public boolean knownKey(final ASAtom key) {
+        return this.entries.containsKey(key);
+    }
     public COSObject getKey(final ASAtom key) {
         return this.entries.get(key);
     }
@@ -31,6 +120,86 @@ public class COSDictionary extends COSDirect {
             this.entries.put(key, value);
         }
         return true;
+    }
+
+    public boolean getBooleanKey(final ASAtom key) {
+        return getKey(key).getBoolean();
+    }
+
+    public boolean setBooleanKey(final ASAtom key, final boolean value) {
+        COSObject obj = new COSObject();
+        obj.setBoolean(value);
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public long getIntegerKey(final ASAtom key) {
+        return getKey(key).getInteger();
+    }
+
+    public boolean setIntegerKey(final ASAtom key, final long value) {
+        COSObject obj = new COSObject();
+        obj.setInteger(value);
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public double getRealKey(final ASAtom key) {
+        return getKey(key).getReal();
+    }
+
+    public boolean setRealKey(final ASAtom key, final double value) {
+        COSObject obj = new COSObject();
+        obj.setReal(value);
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public String getStringKey(final ASAtom key) {
+        return getKey(key).getString();
+    }
+
+    public boolean setStringKey(final ASAtom key, final String value) {
+        COSObject obj = new COSObject();
+        obj.setString(value);
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public final ASAtom getNameKey(final ASAtom key) {
+        return getKey(key).getName();
+    }
+
+    public boolean setNameKey(final ASAtom key, final ASAtom value) {
+        COSObject obj = new COSObject();
+        obj.setName(value);
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public boolean setArrayKey(final ASAtom key) {
+        COSObject obj = new COSObject();
+        obj.setArray();
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public boolean setArrayKey(final ASAtom key, final int size, final COSObject[] value) {
+        COSObject obj = new COSObject();
+        obj.setArray(size, value);
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public boolean setArrayKey(final ASAtom key, final int size, final double[] value) {
+        COSObject obj = new COSObject();
+        obj.setArray(size, value);
+        this.entries.put(key, obj);
+        return true;
+    }
+
+    public void removeKey(final ASAtom key) {
+        this.entries.remove(key);
     }
 
 }
