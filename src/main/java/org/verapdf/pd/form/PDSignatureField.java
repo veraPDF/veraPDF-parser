92e512960531bee46473156fb69116d732f9180e
shem-sergey <sergey.shemyakov@duallab.com>
2016-09-22 12:07:17 +0300

    Added PDSignatureParser.

---
+++ src/main/java/org/verapdf/pd/form/PDSignatureField.java
@@ -0,0 +1,16 @@
+package org.verapdf.pd.form;
+
+import org.verapdf.cos.COSObject;
+
+/**
+ * @author Sergey Shemyakov
+ */
+public class PDSignatureField extends PDFormField {
+
+    public PDSignatureField(COSObject obj) {
+        super(obj);
+    }
+
+
+
+}
