fbc25cc9083f68a9585ae95a094308013b56bccf
shem-sergey <sergey.shemyakov@duallab.com>
2016-10-06 14:09:45 +0300

    Fixed NullPointerException in test.

--- src/test/java/org/verapdf/pd/font/opentype/OpenTypeCFFTest.java
+++ src/test/java/org/verapdf/pd/font/opentype/OpenTypeCFFTest.java
@@ -1,7 +1,10 @@
 package org.verapdf.pd.font.opentype;
 
 import org.junit.Test;
+import org.verapdf.as.ASAtom;
 import org.verapdf.as.io.ASInputStream;
+import org.verapdf.cos.COSName;
+import org.verapdf.cos.COSObject;
 import org.verapdf.io.InternalInputStream;
 import org.verapdf.pd.font.cff.CFFFontProgram;
 
@@ -18,8 +21,9 @@ public class OpenTypeCFFTest {
 
     @Test
     public void test() throws IOException {
+        COSObject encoding = COSName.construct(ASAtom.WIN_ANSI_ENCODING);
         ASInputStream stream = new InternalInputStream(fontFilePath);
-        OpenTypeFontProgram font = new OpenTypeFontProgram(stream, true, false, null);
+        OpenTypeFontProgram font = new OpenTypeFontProgram(stream, true, false, encoding);
         font.parseFont();
         assertTrue(font.getFont() instanceof CFFFontProgram);
         assertTrue(!((CFFFontProgram) font.getFont()).isCIDFont());
