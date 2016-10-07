fb8672cd2a5967661688d12b7685196f2c4f596f
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2016-10-07 14:29:00 +0300

    Resource cache

--- src/main/java/org/verapdf/pd/PDResources.java
+++ src/main/java/org/verapdf/pd/PDResources.java
@@ -10,10 +10,20 @@ import org.verapdf.pd.font.PDFont;
 import org.verapdf.pd.images.PDXObject;
 import org.verapdf.pd.patterns.PDShading;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Timur Kamalov
  */
 public class PDResources extends PDObject {
+
+	private Map<ASAtom, PDColorSpace> colorSpaceMap = new HashMap<>();
+	private Map<ASAtom, PDColorSpace> patternMap = new HashMap<>();
+	private Map<ASAtom, PDShading> shadingMap = new HashMap<>();
+	private Map<ASAtom, PDXObject> xObjectMap = new HashMap<>();
+	private Map<ASAtom, PDExtGState> extGStateMap = new HashMap<>();
+	private Map<ASAtom, PDFont> fontMap = new HashMap<>();
 
 	public PDResources(COSObject resourcesDictionary) {
 		super(resourcesDictionary);
@@ -21,12 +31,18 @@ public class PDResources extends PDObject {
 
 	//TODO : think about error cases
 	public PDColorSpace getColorSpace(ASAtom name) {
+		if (colorSpaceMap.containsKey(name)) {
+			return colorSpaceMap.get(name);
+		}
+		PDColorSpace colorSpace;
 		COSObject rawColorSpace = getResource(ASAtom.COLORSPACE, name);
 		if (rawColorSpace != null && !rawColorSpace.empty()) {
-			return ColorSpaceFactory.getColorSpace(rawColorSpace);
+			colorSpace = ColorSpaceFactory.getColorSpace(rawColorSpace);
 		} else {
-			return ColorSpaceFactory.getColorSpace(COSName.construct(name));
+			colorSpace = ColorSpaceFactory.getColorSpace(COSName.construct(name));
 		}
+		colorSpaceMap.put(name, colorSpace);
+		return colorSpace;
 	}
 
 	public PDColorSpace getDefaultColorSpace(ASAtom name) {
@@ -46,28 +62,54 @@ public class PDResources extends PDObject {
 	}
 
 	public PDColorSpace getPattern(ASAtom name) {
+		if (patternMap.containsKey(name)) {
+			return patternMap.get(name);
+		}
 		COSObject rawPattern = getResource(ASAtom.PATTERN, name);
-		return ColorSpaceFactory.getColorSpace(rawPattern);
+		PDColorSpace pattern = ColorSpaceFactory.getColorSpace(rawPattern);
+		patternMap.put(name, pattern);
+		return pattern;
 	}
 
 	public PDShading getShading(ASAtom name) {
+		if (shadingMap.containsKey(name)) {
+			return shadingMap.get(name);
+		}
 		COSObject rawShading = getResource(ASAtom.SHADING, name);
-		return new PDShading(rawShading);
+		PDShading shading = new PDShading(rawShading);
+		shadingMap.put(name, shading);
+		return shading;
 	}
 
 	public PDXObject getXObject(ASAtom name) {
+		if (xObjectMap.containsKey(name)) {
+			return xObjectMap.get(name);
+		}
 		COSObject rawXObject = getResource(ASAtom.XOBJECT, name);
-		return PDXObject.getTypedPDXObject(rawXObject);
+		PDXObject pdxObject = PDXObject.getTypedPDXObject(rawXObject);
+		xObjectMap.put(name, pdxObject);
+		return pdxObject;
 	}
 
 	public PDExtGState getExtGState(ASAtom name) {
+		if (extGStateMap.containsKey(name)) {
+			return extGStateMap.get(name);
+		}
 		COSObject rawExtGState = getResource(ASAtom.EXT_G_STATE, name);
-		return new PDExtGState(rawExtGState);
+		PDExtGState extGState = new PDExtGState(rawExtGState);
+		extGStateMap.put(name, extGState);
+		return extGState;
 	}
 
 	public PDFont getFont(ASAtom name) {
+		// TODO: check logic in fonts (null pointer case)
+//		if (fontMap.containsKey(name)) {
+//			return fontMap.get(name);
+//		}
 		COSObject rawFont = getResource(ASAtom.FONT, name);
-		return PDFontFactory.getPDFont(rawFont);
+		PDFont font = PDFontFactory.getPDFont(rawFont);
+//		fontMap.put(name, font);
+		return font;
 	}
 
 	private COSObject getResource(ASAtom type, ASAtom name) {
