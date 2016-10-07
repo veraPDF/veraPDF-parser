1bb8a0b3d45815d4826aee0e398044395f061092
shem-sergey <sergey.shemyakov@duallab.com>
2016-10-07 14:39:16 +0300

    Started encryption tools.

---
+++ src/main/java/org/verapdf/tools/EncryptionTools.java
@@ -0,0 +1,15 @@
+package org.verapdf.tools;
+
+/**
+ * Contains methods for encryption and decryption of PDF files.
+ *
+ * @author Sergey Shemyakov
+ */
+public class EncryptionTools {
+
+    public static byte[] computeEncryptionKey(String password, String o, String p,
+                                              byte[] id) {
+
+    }
+
+}
