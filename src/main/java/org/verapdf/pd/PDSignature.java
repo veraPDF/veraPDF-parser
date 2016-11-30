07df272bb09d02b3387a159d094ea0f65676cee5
shem-sergey <sergey.shemyakov@duallab.com>
2016-11-30 12:38:59 +0300

    Added getters for feature extraction.

--- src/main/java/org/verapdf/pd/PDSignature.java
+++ src/main/java/org/verapdf/pd/PDSignature.java
@@ -49,4 +49,56 @@ public class PDSignature extends PDObject {
         COSString contents = (COSString) this.getKey(ASAtom.CONTENTS).get();
         return contents;
     }
+
+    /**
+     * @return the name of the preferred signature handler to use when
+     * validating this signature.
+     */
+    public ASAtom getFilter() {
+        return this.getNameKey(ASAtom.FILTER);
+    }
+
+    /**
+     * @return a name that describes the encoding of the signature value and key
+     * information in the signature dictionary.
+     */
+    public ASAtom getSubfilter() {
+        return this.getNameKey(ASAtom.SUB_FILTER);
+    }
+
+    /**
+     * @return the name of the person or authority signing the document.
+     */
+    public String getName() {
+        return this.getStringKey(ASAtom.NAME);
+    }
+
+    /**
+     * @return the time of signing.
+     */
+    public String getSignDate() {
+        return this.getStringKey(ASAtom.M);
+    }
+
+    /**
+     * @return the CPU host name or physical location of the signing.
+     */
+    public String getLocation() {
+        return this.getStringKey(ASAtom.LOCATION);
+    }
+
+    /**
+     * @return the reason for the signing, such as ( I agree ).
+     */
+    public String getReason() {
+        return this.getStringKey(ASAtom.REASON);
+    }
+
+    /**
+     * @return information provided by the signer to enable a recipient to
+     * contact the signer to verify the signature.
+     */
+    public String getContactInfo() {
+        return this.getStringKey(ASAtom.CONTACT_INFO);
+    }
 }
