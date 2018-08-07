7e7427721c24eea23853e8613121fd26c2e51c81
Maksim Bezrukov <maksim.bezrukov@duallab.com>
2018-08-07 16:25:44 +0300

    Fixed warning message logging for font factory

--- src/main/java/org/verapdf/factory/fonts/PDFontFactory.java
+++ src/main/java/org/verapdf/factory/fonts/PDFontFactory.java
@@ -50,24 +50,26 @@ public class PDFontFactory {
      * @return PDFont that corresponds to this dictionary.
      */
     public static PDFont getPDFont(COSObject fontDictionary) {
-        if (fontDictionary != null && fontDictionary.getType() == COSObjType.COS_DICT) {
-            COSDictionary dict = (COSDictionary) fontDictionary.getDirectBase();
-            ASAtom subtype = fontDictionary.getNameKey(ASAtom.SUBTYPE);
-            if (subtype == ASAtom.TYPE1 ||
-                    subtype == ASAtom.MM_TYPE1) {
-                return new PDType1Font(dict);
-            } else if (subtype == ASAtom.TRUE_TYPE) {
-                return new PDTrueTypeFont(dict);
-            } else if (subtype == ASAtom.TYPE3) {
-                return new PDType3Font(dict);
-            } else if (subtype == ASAtom.TYPE0) {
-                return new PDType0Font(dict);
-            } else {
-                LOGGER.log(Level.FINE, "Invalid value of Subtype in font dictionary");
-                return null;
+        if (fontDictionary != null) {
+            if (fontDictionary.getType() == COSObjType.COS_DICT) {
+                COSDictionary dict = (COSDictionary) fontDictionary.getDirectBase();
+                ASAtom subtype = fontDictionary.getNameKey(ASAtom.SUBTYPE);
+                if (subtype == ASAtom.TYPE1 ||
+                        subtype == ASAtom.MM_TYPE1) {
+                    return new PDType1Font(dict);
+                } else if (subtype == ASAtom.TRUE_TYPE) {
+                    return new PDTrueTypeFont(dict);
+                } else if (subtype == ASAtom.TYPE3) {
+                    return new PDType3Font(dict);
+                } else if (subtype == ASAtom.TYPE0) {
+                    return new PDType0Font(dict);
+                } else {
+                    LOGGER.log(Level.FINE, "Invalid value of Subtype in font dictionary");
+                    return null;
+                }
             }
+            LOGGER.log(Level.SEVERE, "Font object is not a dictionary");
         }
-		LOGGER.log(Level.SEVERE, "Font object is not a dictionary");
 		return null;
     }
 }
