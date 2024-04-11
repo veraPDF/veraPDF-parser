91b3ab1d6d24e4f7a39f9902b8ba5a365d40b189
Maxim <maxplusim@mail.ru>
2024-04-11 10:32:46 +0300

    Refactoring flavours. Add WTPDF flavours

--- src/main/java/org/verapdf/parser/PDFFlavour.java
+++ src/main/java/org/verapdf/parser/PDFFlavour.java
@@ -15,8 +15,10 @@ public enum PDFFlavour {
 	PDFA_4_E(Specification.ISO_19005_4),
 	PDFUA_1(Specification.ISO_14289_1),
 	PDFUA_2(Specification.ISO_14289_2),
-	WCAG2_1(Specification.WCAG2_1),
-	WCAG2_2(Specification.WCAG2_2);
+	WTPDF_1_0_REUSE(Specification.WTPDF_1_0),
+	WTPDF_1_0_ACCESSIBILITY(Specification.WTPDF_1_0),
+	WCAG_2_1(Specification.WCAG_2_1),
+	WCAG_2_2(Specification.WCAG_2_2);
 
 	private final Specification specification;
 
@@ -29,24 +31,31 @@ public enum PDFFlavour {
 	}
 
 	public enum Specification {
-		NO_STANDARD(SpecificationFamily.NONE),
-		ISO_14289_1(SpecificationFamily.PDF_UA),
-		ISO_14289_2(SpecificationFamily.PDF_UA),
-		ISO_19005_1(SpecificationFamily.PDF_A),
-		ISO_19005_2(SpecificationFamily.PDF_A),
-		ISO_19005_3(SpecificationFamily.PDF_A),
-		ISO_19005_4(SpecificationFamily.PDF_A),
-		WCAG2_1(SpecificationFamily.WCAG),
-		WCAG2_2(SpecificationFamily.WCAG);
+		NO_STANDARD(SpecificationFamily.NONE, PDFSpecification.NO_SPECIFICATION),
+		ISO_14289_1(SpecificationFamily.PDF_UA, PDFSpecification.ISO_32000_1_7),
+		ISO_14289_2(SpecificationFamily.PDF_UA, PDFSpecification.ISO_32000_2_0),
+		ISO_19005_1(SpecificationFamily.PDF_A, PDFSpecification.PDF_REFERENCE_1_4),
+		ISO_19005_2(SpecificationFamily.PDF_A, PDFSpecification.ISO_32000_1_7),
+		ISO_19005_3(SpecificationFamily.PDF_A, PDFSpecification.ISO_32000_1_7),
+		ISO_19005_4(SpecificationFamily.PDF_A, PDFSpecification.ISO_32000_2_0),
+		WTPDF_1_0(SpecificationFamily.WTPDF, PDFSpecification.ISO_32000_2_0),
+		WCAG_2_1(SpecificationFamily.WCAG, PDFSpecification.ISO_32000_2_0),
+		WCAG_2_2(SpecificationFamily.WCAG, PDFSpecification.ISO_32000_2_0);
 
 		private final SpecificationFamily family;
+		private final PDFSpecification pdfSpecification;
 
-		Specification(final SpecificationFamily family) {
+		Specification(final SpecificationFamily family, final PDFSpecification pdfSpecification) {
 			this.family = family;
+			this.pdfSpecification = pdfSpecification;
 		}
 		
 		public SpecificationFamily getFamily() {
 			return family;
+		}
+
+		public PDFSpecification getPdfSpecification() {
+			return pdfSpecification;
 		}
 	}
 	
@@ -54,6 +63,34 @@ public enum PDFFlavour {
 		NONE,
 		PDF_A,
 		PDF_UA,
-		WCAG
+		WCAG,
+		WTPDF
 	}
-}
+
+	public enum PDFSpecification {
+		NO_SPECIFICATION,
+		PDF_REFERENCE_1_4,
+		ISO_32000_1_7,
+		ISO_32000_2_0
+	}
+	
+	public static boolean isFlavourPDFSpecification(PDFFlavour flavour, PDFSpecification pdfSpecification) {
+		return flavour != null && flavour.getSpecification().getPdfSpecification() == pdfSpecification;
+	}
+
+	public static boolean isFlavourFamily(PDFFlavour flavour, SpecificationFamily specificationFamily) {
+		return flavour != null && flavour.getSpecification().getFamily() == specificationFamily;
+	}
+
+	public static boolean isFlavour(PDFFlavour currentFlavour, PDFFlavour flavour) {
+		return currentFlavour != null && currentFlavour == flavour;
+	}
+
+	public static boolean isFlavourPart(PDFFlavour flavour, Specification specificationPart) {
+		return flavour != null && flavour.getSpecification() == specificationPart;
+	}
+
+	public static boolean isPDFUA2RelatedFlavour(PDFFlavour flavour) {
+		return isFlavour(flavour, PDFFlavour.PDFUA_2) || isFlavourPart(flavour, Specification.WTPDF_1_0);
+	}
+} 
