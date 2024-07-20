00ac0852966732500ca1bb51b1f0f27ff46408fb
Maxim <maxplusim@mail.ru>
2024-07-20 18:12:06 +0300

    Update licence headers

--- src/main/java/org/verapdf/cos/COSStream.java
+++ src/main/java/org/verapdf/cos/COSStream.java
@@ -1,6 +1,6 @@
 /**
  * This file is part of veraPDF Parser, a module of the veraPDF project.
- * Copyright (c) 2015, veraPDF Consortium <info@verapdf.org>
+ * Copyright (c) 2015-2024, veraPDF Consortium <info@verapdf.org>
  * All rights reserved.
  *
  * veraPDF Parser is free software: you can redistribute it and/or modify
