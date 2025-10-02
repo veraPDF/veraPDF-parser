f95b70c9121275296cf5a3f84ff29159d7aaf62d
Kakhnovich Raman <59254003+LonelyMidoriya@users.noreply.github.com>
2025-10-02 16:31:10 +0300

    Add support of the /VE tag in OCMD (#684)
    * Add support of the /VE tag in OCMD

--- src/main/java/org/verapdf/pd/optionalcontent/PDOCMDDictionary.java
+++ src/main/java/org/verapdf/pd/optionalcontent/PDOCMDDictionary.java
@@ -1,12 +1,14 @@
 package org.verapdf.pd.optionalcontent;
 
 import org.verapdf.as.ASAtom;
-import org.verapdf.cos.COSArray;
-import org.verapdf.cos.COSBase;
-import org.verapdf.cos.COSObjType;
-import org.verapdf.cos.COSObject;
+import org.verapdf.cos.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PDOCMDDictionary {
+    private static final Logger LOGGER = Logger.getLogger(PDOCMDDictionary.class.getCanonicalName());
+
     public static boolean isVisibleOCMDByP(COSBase property, PDOptionalContentProperties optProperties) {
         COSObject ocgProperty = property.getKey(ASAtom.OCGS);
         if (ocgProperty == null || ocgProperty.getType() != COSObjType.COS_ARRAY) {
@@ -33,5 +35,68 @@ public class PDOCMDDictionary {
             }
         }
         return ASAtom.ALL_OFF.equals(pValue) || ASAtom.ALL_ON.equals(pValue);
+    }
+
+    public static boolean isVisibleOCMD(COSBase property, PDOptionalContentProperties optProperties) {
+        COSObject veProperty = property.getKey(ASAtom.VE);
+        if (veProperty != null && veProperty.getType() == COSObjType.COS_ARRAY) {
+            COSArray veArray = (COSArray) veProperty.getDirectBase();
+            return evaluateVE(veArray, optProperties);
+        }
+
+        return isVisibleOCMDByP(property, optProperties);
+    }
+
+    private static boolean evaluateVE(COSArray expr, PDOptionalContentProperties optProperties) {
+        if (expr.size() == 0) {
+            return true;
+        }
+
+        COSBase first = expr.at(0).getDirectBase();
+
+        if (first instanceof COSName) {
+            ASAtom operator = first.getName();
+
+            if (ASAtom.OR.equals(operator)) {
+                for (int i = 1; i < expr.size(); i++) {
+                    if (evaluateOperandOfVE(expr.at(i).getDirectBase(), optProperties)) {
+                        return true;
+                    }
+                }
+                return false;
+            } else if (ASAtom.AND.equals(operator)) {
+                for (int i = 1; i < expr.size(); i++) {
+                    if (!evaluateOperandOfVE(expr.at(i).getDirectBase(), optProperties)) {
+                        return false;
+                    }
+                }
+                return true;
+            } else if (ASAtom.NOT.equals(operator)) {
+                if (expr.size() != 2) {
+                    LOGGER.log(Level.WARNING, "/Not operator should have only 1 argument in VE array");
+                    return true;
+                }
+                return !evaluateOperandOfVE(expr.at(1).getDirectBase(), optProperties);
+            }
+            LOGGER.log(Level.WARNING, String.format("First element of VE array has value %s instead of /And, /Or or /Not", operator));
+        } else {
+            LOGGER.log(Level.WARNING, "First element of VE array should have type name");
+        }
+
+        return true;
+    }
+
+
+    private static boolean evaluateOperandOfVE(COSBase operand, PDOptionalContentProperties optProperties) {
+        if (operand instanceof COSArray) {
+            return evaluateVE((COSArray) operand, optProperties);
+
+        } else if (operand instanceof COSDictionary) {
+            COSDictionary dict = (COSDictionary) operand;
+            String name = dict.getStringKey(ASAtom.NAME);
+            return optProperties.isVisibleLayer(name);
+        }
+
+        return true;
     }
 }
